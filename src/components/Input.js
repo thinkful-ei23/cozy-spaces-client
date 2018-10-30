c8245c9c33c5ff61f6fa64493d4f0e3ddd74787d
Burkey Koontz <bk.koontz@gmail.com>
2018-10-30 14:39:29 -0700

    added a line break

--- src/components/Input.js
+++ src/components/Input.js
@@ -33,6 +33,7 @@ export default class Input extends React.Component {
                     {error}
                     {warning}
                 </div>
+                <br />
             </div>
         );
     }
