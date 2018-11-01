63cde7d8d8205fe442d477f0ae937163ca09a100
Alex Puhl <apuhlswimmer@hotmail.com>
2018-11-01 11:04:57 -0600

    added jwt refresh feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Route, withRouter } from 'react-router-dom';
 
 import Header from './Header';
@@ -9,20 +9,50 @@ import Profile from './Profile';
 import AddAListingPage from './AddAListingPage';
 import LandingPage from './LandingPage';
 import Listing from './Listing';
-import Dashboard from "./Dashboard";
+import Dashboard from './Dashboard';
+import { refreshAuthToken } from '../actions/auth';
 
 class App extends Component {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      60 * 60 * 1000 // One hour
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+
+    clearInterval(this.refreshInterval);
+  }
+
   render() {
     return (
       <div className="app">
-          <Header />
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/register" component={Registration} />
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/add-listing" component={AddAListingPage} />
-          <Route exact path="/places/:id" component={Listing} />
+        <Header />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={Registration} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/add-listing" component={AddAListingPage} />
+        <Route exact path="/places/:id" component={Listing} />
       </div>
     );
   }
