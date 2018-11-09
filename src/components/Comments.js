12c9b669e46948cb4c3ab97c860fb2612fd48a61
Selvin Norman <selvin715@gmail.com>
2018-11-09 12:34:20 -0800

    removed unused import

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { access } from 'fs';
 
 class Comments extends React.Component {
 
