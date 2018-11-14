c6ab9d3ee70785cdccf8cd659aa35eaa20a89071
Burkey Koontz <bk.koontz@gmail.com>
2018-11-14 07:50:09 -0800

    got rid of comment

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -45,6 +45,7 @@ class Dashboard extends Component {
     } else {
       errors['zip'] = 'Please enter a valid zip code';
       this.setState({errors: errors})
+      console.log(false);
     }
   }
 
@@ -115,6 +116,7 @@ class Dashboard extends Component {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
+    console.log(pos);
     this.setState({ currentLocation: pos, geoLocationError: false });
     this.props.dispatch(fetchPlaces(pos));
     // send this filter to get places
@@ -155,9 +157,9 @@ class Dashboard extends Component {
             >
             <img
                 alt={`${place.photos[0].caption}`}
-                className='main-orange-bottom-border'
                 src={`${place.photos[0].url}`}
                 />
+              {/* This needs to be commented out temporarily because new listings don't have photos */}
               <div className="place-card-content">
                 <h2 className="inline name">{place.name}</h2>
                 <div className="place-card-content-r1">
